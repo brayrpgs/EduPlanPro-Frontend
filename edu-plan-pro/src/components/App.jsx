4d44d80472220502f0db8829518ed8c10afcfd63
KendallFallas07 <KendallFallas2018@gmail.com>
2024-10-14 01:22:32 -0600

    implementacion del enrutado y el login al 50%

---
+++ edu-plan-pro/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";  
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import {Login} from "./login/Login.js"
+
+function App() {
+  return (
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/login"/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    
+    </BrowserRouter>
+  );
+}
+
+export default App;
