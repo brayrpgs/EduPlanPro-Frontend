7337fa50b28fea8f56408cd0bd660442c1d162bc
Ceasar Calvo <ceasar.calvo.munoz@una.ac.cr>
2025-04-02 01:11:33 -0600

    Task 66 20%

--- edu-plan-pro/src/index.js
+++ edu-plan-pro/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import "./index.css"
+//import "./index.css"
 
+if(false){
+    require( "./index.css")
+}else{
+    require( "./indexx.css")
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
