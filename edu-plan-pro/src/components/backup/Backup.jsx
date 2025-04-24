e5c02a3165f03e67de524234e8f5ba06213c34fd
BrayRpgs <brayanymelquisedec@gmail.com>
2025-04-23 23:33:27 -0600

    Add Backup component with initial structure and documentation
    Brayan rosales perez
    23-04-2025

---
+++ edu-plan-pro/src/components/backup/Backup.jsx
@@ -0,0 +1,18 @@
+
+/**
+ * @description This is the component for the backup.
+ * @author Brayan rosales perez
+ * 2024-04-23
+ * @returns 
+ */
+export default function Backup() {
+    /**
+     * 
+     */
+    return (
+        <div>
+            <h1>Backup</h1>
+            <p>This is the backup page.</p>
+        </div>
+    );
+}
