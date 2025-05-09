50346ef6a9c6fbf73701f0f566c6ddb11b2636c3
BrayRpgs <brayanymelquisedec@gmail.com>
2025-05-09 03:25:26 -0600

    -- create Item into dashboard -- create Icon JSX -- date 08-05-2025

---
+++ edu-plan-pro/src/components/icons/DashboardIcons/RecycleIcon.jsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+function RecycleIcon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="85%"
+            viewBox="0 -960 960 960"
+            width="85%"
+        >
+            <path d="M440-320h80v-166l64 62 56-56-160-160-160 160 56 56 64-62v166zM280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280zm400-600H280v520h400v-520zm-400 0v520-520z" />
+        </svg>
+    )
+}
+
+export default RecycleIcon
