b3d05ba55129706782dd74ec053c8979e4dff527
BrayRpgs <brayanymelquisedec@gmail.com>
2025-05-11 23:21:24 -0600

    -- add table -- add search and clean fields -- date : 11-05-2025

--- edu-plan-pro/src/components/recyclingbin/RecyclingBin.jsx
+++ edu-plan-pro/src/components/recyclingbin/RecyclingBin.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Header from '../header/Header'
 import Footer from '../footer/Footer'
-import { RecyclingBinHead } from './RecyclingBinTable'
+import { RecyclingBinHead } from './RecyclingBinHead'
+import { RecyclingBinSearch } from './RecyclingBinSearch'
+import { RecyclingBinTable } from './RecyclingBinTable'
 
 export const RecyclingBin = () => {
     return (
         <div className="grid grid-rows-[auto_1fr_auto] min-h-screen h-screen">
             <Header />
-            <RecyclingBinHead />
+            <div className='pr-[15vw] pl-[15vw]'>
+                <RecyclingBinHead />
+                <RecyclingBinSearch />
+                <RecyclingBinTable />
+            </div>
             <Footer />
         </div>
     )
--- edu-plan-pro/src/components/recyclingbin/RecyclingBinTable.jsx
+++ edu-plan-pro/src/components/recyclingbin/RecyclingBinTable.jsx
@@ -1,46 +1,38 @@
 import React from 'react'
 
-export const RecyclingBinHead = () => {
+export const RecyclingBinTable = () => {
     return (
-        <div className='pr-[15vw] pl-[15vw]'>
-            <div className='bg-UNA-Blue-Dark w-[100%] flex rounded-[0.5vh] items-center text-white mt-[3vh] p-[1vh] justify-around'>
-                <h1 className='ml-[1vw] my-[0.5vh] text-xl text-white text-center'>Papelera de reciclaje</h1>
-                <div className='flex flex-col justify-center content-center items-center w-[8vw]'>
-                    <label htmlFor="school">Escuela</label>
-                    <input id='school' type="checkbox" />
-                </div>
+        <div className="flex justify-center items-center mt-0 w-full overflow-x-auto">
+            <table className="w-full table-auto">
+                <thead>
+                    <tr>
+                        <th className="border-[0.1vh] border-gray-400 px-[1vw] py-[1vh] text-center text-[1vw] text-UNA-Red">
+                            Elementos
+                        </th>
+                        <th className="border-[0.1vh] border-gray-400 px-[1vw] py-[1vh] text-center text-[1vw] text-UNA-Red">
+                            Modificado
+                        </th>
+                        <th className="border-[0.1vh] border-gray-400 px-[1vw] py-[1vh] w-[10vw] text-[1vw] text-UNA-Red">
+                            Acciones
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr key={"1"}>
+                        <td className="border-[0.1vh] border-gray-400 px-[1vw] py-[1.5vh] text-[0.9vw] text-center items-center break-words whitespace-normal max-w-[15vw]">
+                            {"dato"}
+                        </td>
+                        <td className="border-[0.1vh] border-gray-400 px-[1vw] py-[1.5vh] text-[0.9vw] text-center items-center break-words whitespace-normal max-w-[15vw]">
+                            {"dato"}
+                        </td>
+                        <td className="border-[0.1vh] border-gray-400 px-[1vw] py-[1vh] text-[0.9vw]">
+                            <div className="flex items-center flex-row justify-center w-full h-full gap-[0.2vw]">
 
-                <div className='flex flex-col justify-center content-center items-center text-center '>
-                    <label htmlFor="faculty">Facultad</label>
-                    <input id='faculty' type="checkbox" className='text-white' />
-                </div>
-
-                <div className='flex flex-col justify-center content-center items-center text-center '>
-                    <label htmlFor="carrer">Carrera</label>
-                    <input id='carrer' type="checkbox" />
-                </div>
-
-
-                <div className='flex flex-col justify-center content-center items-center text-center'>
-                    <label htmlFor="profesor">Docente</label>
-                    <input id='profesor' type="checkbox" />
-                </div>
-
-                <div className='flex flex-col justify-center content-center items-center text-center '>
-                    <label htmlFor="user">Usuario</label>
-                    <input id='user' type="checkbox" />
-                </div>
-
-                <div className='flex flex-col justify-center content-center items-center text-center '>
-                    <label htmlFor="study-plan">Plan de estudio</label>
-                    <input id='study-plan' type="checkbox" />
-                </div>
-
-                <div className='flex flex-col justify-center content-center items-center text-center '>
-                    <label htmlFor="course-program">Programa de cursos</label>
-                    <input id='course-program' type="checkbox" />
-                </div>
-            </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
