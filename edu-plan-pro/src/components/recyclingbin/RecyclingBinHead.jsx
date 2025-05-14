1fd3c2c0243186c76d52dce1d181564c306d4ce1
BrayRpgs <brayanymelquisedec@gmail.com>
2025-05-14 03:26:57 -0600

    -- add school , faculty and carreer to recycle bin -- date 14-05-2025

--- edu-plan-pro/src/components/recyclingbin/RecyclingBinHead.jsx
+++ edu-plan-pro/src/components/recyclingbin/RecyclingBinHead.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react';
 import data from './fields.json'
 
-export const RecyclingBinHead = () => {
+export const RecyclingBinHead = ({ setData, datas }) => {
 
     /**optiene los datos de un json llamado
      * fields.json en el mismo directorio
      */
     const fields = data
+
+    const [school, setSchool] = useState([])
+    const [faculty, setFaculty] = useState([])
+    const [carrer, setCarrer] = useState([])
+    const [profesor, setProfesor] = useState([])
+    const [user, setUser] = useState([])
+    const [studyPlan, setStudyPlan] = useState([])
+    const [courseProgram, setCourseProgram] = useState([])
+
+    useEffect(() => {
+        setData(school.concat(faculty))
+    }, [school, faculty, carrer, profesor, user, studyPlan, courseProgram])
 
 
     /**
@@ -25,8 +38,144 @@ export const RecyclingBinHead = () => {
         }
     }
 
-    function handleFilterCheckbox(event) {
-        console.log(event.target.checked)
+    function cleanDataSchool(origin) {
+        const newOrigin = origin.map((value) => (
+            {
+                "data": value["NOMBRE ESCUELA"],
+                "id": value["ID_SCHOOL"],
+                "module": "Escuela"
+            })
+        )
+        return newOrigin
+    }
+
+    function cleanDataFaculty(origin) {
+        const newOrigin = origin.map((value) => (
+            {
+                "data": value["NOMBRE FACULTAD"],
+                "id": value["ID_FACULTY"],
+                "module": "Facultad"
+            })
+        )
+        return newOrigin
+    }
+
+    function cleanDataCarreer(origin) {
+        const newOrigin = origin.map((value) => (
+            {
+                "data": value["NOMBRE DE CARRERA"],
+                "id": value["ID_CAREER"],
+                "module": "Carrera"
+            })
+        )
+        return newOrigin
+    }
+
+
+    async function handleFilterCheckbox(event) {
+
+        console.log(event.target.id);
+        console.log(event.target.checked);
+
+        //para traer y setear los datos de las ecuelas
+        if (event.target.id === "school") {
+            if (event.target.checked) {
+                const req = await (await fetch(
+                    fields[0].url,
+                    {
+                        credentials: "include",
+                    }
+                )).json()
+                setSchool(cleanDataSchool(req.data))
+            } else {
+                setSchool([])
+            }
+        }
+
+        //para facultades
+        if (event.target.id === "faculty") {
+            if (event.target.checked) {
+                const req = await (await fetch(
+                    fields[1].url,
+                    {
+                        credentials: "include",
+                    }
+                )).json()
+                setFaculty(cleanDataFaculty(req.data))
+            } else {
+                setFaculty([])
+            }
+        }
+        //para carreras 
+        if (event.target.id !== "carrer") {
+            if (event.target.checked) {
+                const req = await (await fetch(
+                    fields[2].url,
+                    {
+                        credentials: "include",
+                    }
+                )).json()
+                setCarrer(cleanDataCarreer(req.data))
+            } else {
+                setCarrer([])
+            }
+        }
+        //para profesores
+        if (event.target.id !== "profesor") {
+            if (event.target.checked) {
+                const req = await (await fetch(
+                    fields[3].url,
+                    {
+                        credentials: "include",
+                    }
+                )).json()
+                setProfesor(req.data)
+            } else {
+                setProfesor([])
+            }
+        }
+        //para usuarios
+        if (event.target.id !== "user") {
+            if (event.target.checked) {
+                const req = await (await fetch(
+                    fields[4].url,
+                    {
+                        credentials: "include",
+                    }
+                )).json()
+                setUser(req.data)
+            } else {
+                setUser([])
+            }
+        }
+        //para planes de estudio
+        if (event.target.id !== "study-plan") {
+            if (event.target.checked) {
+                const req = await (await fetch(
+                    fields[5].url,
+                    {
+                        credentials: "include",
+                    }
+                )).json()
+                setStudyPlan(req.data)
+            } else {
+                setStudyPlan([])
+            }
+        }
+        //para programas del curso
+        if (event.target.id !== "course-program") {
+            if (event.target.checked) {
+                const req = await (await fetch(
+                    fields[6].url,
+                    {
+                        credentials: "include",
+                    }
+                )).json()
+                setCourseProgram(req.data)
+            } else {
+                setCourseProgram([])
+            }
+        }
     }
 
     return (
--- edu-plan-pro/src/components/recyclingbin/RecyclingBinTable.jsx
+++ edu-plan-pro/src/components/recyclingbin/RecyclingBinTable.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const RecyclingBinTable = () => {
+export const RecyclingBinTable = ({ data, setData }) => {
     return (
         <div className=' h-[70vh] overflow-y-auto'>
             <div className="flex justify-center items-center mt-0 w-full overflow-x-auto ">
@@ -11,7 +11,7 @@ export const RecyclingBinTable = () => {
                                 Elementos
                             </th>
                             <th className="border-[0.1vh] border-gray-400 px-[1vw] py-[1vh] text-center text-[1vw] text-UNA-Red">
-                                Modificado
+                                Categoria
                             </th>
                             <th className="border-[0.1vh] border-gray-400 px-[1vw] py-[1vh] w-[10vw] text-[1vw] text-UNA-Red">
                                 Acciones
@@ -19,19 +19,24 @@ export const RecyclingBinTable = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr key={"1"}>
-                            <td className="border-[0.1vh] border-gray-400 px-[1vw] py-[1.5vh] text-[0.9vw] text-center items-center break-words whitespace-normal max-w-[15vw]">
-                                {"dato"}
-                            </td>
-                            <td className="border-[0.1vh] border-gray-400 px-[1vw] py-[1.5vh] text-[0.9vw] text-center items-center break-words whitespace-normal max-w-[15vw]">
-                                {"dato"}
-                            </td>
-                            <td className="border-[0.1vh] border-gray-400 px-[1vw] py-[1vh] text-[0.9vw]">
-                                <div className="flex items-center flex-row justify-center w-full h-full gap-[0.2vw]">
+                        {
+                            data.map((value, index) => (
+                                <tr key={index}>
+                                    <td className="border-[0.1vh] border-gray-400 px-[1vw] py-[1.5vh] text-[0.9vw] text-center items-center break-words whitespace-normal max-w-[15vw]">
+                                        {value.data}
+                                    </td>
+                                    <td className="border-[0.1vh] border-gray-400 px-[1vw] py-[1.5vh] text-[0.9vw] text-center items-center break-words whitespace-normal max-w-[15vw]">
+                                        {value.module}
+                                    </td>
+                                    <td className="border-[0.1vh] border-gray-400 px-[1vw] py-[1vh] text-[0.9vw]">
+                                        <div className="flex items-center flex-row justify-center w-full h-full gap-[0.2vw]">
 
-                                </div>
-                            </td>
-                        </tr>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+
                     </tbody>
                 </table>
             </div>
